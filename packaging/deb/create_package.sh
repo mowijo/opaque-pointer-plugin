8bfc11e09e2753c0f1c98cb2b8ff834f4d13e806
Morten W. Jørgensen <morten@winkler.dk>
2014-10-03 13:50:31 +0200

    Made the packageing script toelrant to diffrent working directories.

--- packaging/deb/create_package.sh
+++ packaging/deb/create_package.sh
@@ -1,5 +1,4 @@
-  
-
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 WD=/tmp/mkdeb
 USR_SHARE_DOCS=$WD"/usr/share/doc/qtcreator-plugin-wizard-opaque-class"
@@ -7,18 +6,18 @@ CHANGE_LOG_FILE=$WD"/usr/share/doc/qtcreator-plugin-wizard-opaque-class/changelo
 rm -rf $WD
 mkdir -p $WD/usr/share/qtcreator/templates/wizards/opaque-class
 mkdir -p $USR_SHARE_DOCS
-cp ../../src/* $WD/usr/share/qtcreator/templates/wizards/opaque-class/
-cp -rf DEBIAN $WD
-cp ../../copyright.txt $USR_SHARE_DOCS/copyright
+cp $DIR/../../src/* $WD/usr/share/qtcreator/templates/wizards/opaque-class/
+cp -rf $DIR/DEBIAN $WD
+cp $DIR/../../copyright.txt $USR_SHARE_DOCS/copyright
 find $WD -type d | xargs chmod 755 
 #gzip -c changelog.txt > $CHANGE_LOG_FILE
-gzip --best -c ../../changelog.txt > changelog.Debian.gz
+gzip --best -c $DIR/../../changelog.txt > changelog.Debian.gz
 mv changelog.Debian.gz $USR_SHARE_DOCS
 chmod 0644 $USR_SHARE_DOCS/*
 fakeroot dpkg-deb --build $WD
 
-VERSION=`cat DEBIAN/control | grep Version | awk '{print $2}'`
-PACKAGENAME=`cat DEBIAN/control | grep Package | awk '{print $2}'`
-ARCH=`cat DEBIAN/control | grep Architecture | awk '{print $2}'`
+VERSION=`cat $DIR/DEBIAN/control | grep Version | awk '{print $2}'`
+PACKAGENAME=`cat $DIR/DEBIAN/control | grep Package | awk '{print $2}'`
+ARCH=`cat $DIR/DEBIAN/control | grep Architecture | awk '{print $2}'`
 FILENAME=$PACKAGENAME"_"$VERSION"_"$ARCH".deb"
 mv $WD/../mkdeb.deb $FILENAME
